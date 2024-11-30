84eccd01754b799cb0f95b656b37c356b4383197
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-30 14:50:05 +0900

    feat(OutputView): 제품들을 출력하는 기능 구현

--- src/main/kotlin/store/OutputView.kt
+++ src/main/kotlin/store/OutputView.kt
@@ -1,7 +1,25 @@
 package store
 
+import java.text.DecimalFormat
+
 class OutputView {
     fun printStart() {
         println("안녕하세요. W편의점입니다.")
+    }
+
+    fun printProducts(products: List<Product>) {
+        products.forEach { product ->
+            println("- ${product.name} ${product.price.wonFormat()}원 ${product.quantity.emptyQuantityMessage()}개 ${product.promotion ?: ""}")
+        }
+    }
+
+    private fun Int.wonFormat(): String {
+        val formatter = DecimalFormat("#,###")
+        return formatter.format(this)
+    }
+
+    private fun Int.emptyQuantityMessage(): String {
+        if (this == 0) return "재고 없음"
+        return this.toString()
     }
 }
