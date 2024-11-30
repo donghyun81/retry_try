3ce48ada11c37063b49bf954802737048a76979d
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-30 14:20:00 +0900

    feat(StoreController): 파일을 읽어서 제품들을 생성하는 기능 추가

--- src/main/kotlin/store/StoreController.kt
+++ src/main/kotlin/store/StoreController.kt
@@ -1,9 +1,26 @@
 package store
 
+import java.io.FileReader
+
 class StoreController {
     private val outputView = OutputView()
 
     fun run() {
         outputView.printStart()
+        val products = getProducts()
+    }
+
+    private fun getProducts(): List<Product> {
+        val file = FileReader("src/main/resources/products.md").readLines().drop(1).map { productsInput ->
+            createProduct(productsInput)
+        }
+        return file
+    }
+
+    private fun createProduct(productsInput: String): Product {
+        val (name, priceInput, quantityInput, promotion) = productsInput.split(",")
+        val price = requireNotNull(priceInput.toIntOrNull()) { "[ERROR] 파일 형식이 잘못되었습니다." }
+        val quantity = requireNotNull(quantityInput.toIntOrNull()) { "[ERROR] 파일 형식이 잘못되었습니다." }
+        return Product(name, price, quantity, promotion)
     }
 }
