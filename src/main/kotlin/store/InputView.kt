024a4cad348ab3ce8939d646b5da25b10f8388e5
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-12-07 12:08:43 +0900

    feat(InputView):구매할 상품들과 개수 입력 기능 추가

---
+++ src/main/kotlin/store/InputView.kt
@@ -0,0 +1,29 @@
+package store
+
+import camp.nextstep.edu.missionutils.Console
+
+class InputView {
+    fun readPurchaseProducts(products: List<Product>): List<PurchaseProduct> {
+        println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])")
+        val purchaseProductInput = Console.readLine().split(",")
+        val confirmedPurchaseProductInput = purchaseProductInput.map { input ->
+            require(input.first() == '[' && input.last() == ']') { "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요." }
+            input.removeSurrounding("[", "]")
+        }
+        val purchaseProducts = confirmedPurchaseProductInput.map { input ->
+            require(input.split("-").size == 2) { "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요." }
+            val (name, countInput) = input.split("-")
+            val count = requireNotNull(countInput.toIntOrNull()) { "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요." }
+            PurchaseProduct(name, count)
+        }.groupBy { it.name }.map { (name, product) ->
+            val totalCount = product.sumOf { it.count }
+            PurchaseProduct(name, totalCount)
+        }
+        purchaseProducts.forEach { purchaseProduct ->
+            val stock = products.filter { purchaseProduct.name == it.name }
+            require(stock.isEmpty()) { "[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요." }
+            require(purchaseProduct.count <= stock.sumOf { it.quantity }) { "[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요." }
+        }
+        return purchaseProducts
+    }
+}
