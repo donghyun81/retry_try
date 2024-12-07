b1385e8c2f768fb4efe3f111ed747ba3c15f0129
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-12-07 13:04:38 +0900

    feat(Store): 프로모션이 제외된 제품들을 구하는 기능 구현

--- src/main/kotlin/store/Store.kt
+++ src/main/kotlin/store/Store.kt
@@ -27,4 +27,14 @@ class Store(private val products: List<Product>, private val promotions: List<Pr
         }
         return purchaseProduct.copy(count = 0)
     }
+
+    fun getExcludeProduct(purchaseProduct: RequestProduct): RequestProduct {
+        val promotionStock =
+            products.find { product -> purchaseProduct.name == product.name } ?: return purchaseProduct.copy(count = 0)
+        val promotion = promotions.find { it.name == promotionStock.name } ?: return purchaseProduct.copy(count = 0)
+        val totalEventCount = promotion.buy + promotion.get
+        val promotionCount = purchaseProduct.count.div(totalEventCount) * totalEventCount
+        if (purchaseProduct.count > promotionStock.quantity) return purchaseProduct.copy(count = purchaseProduct.count - promotionCount)
+        return purchaseProduct.copy(count = 0)
+    }
 }
