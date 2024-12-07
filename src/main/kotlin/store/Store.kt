9494490201906ea3d14cd8b88082968eb4272129
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-12-07 12:42:44 +0900

    feat(Store): 구매하는 제품이 프로모션인지 여부를 반환하는 기능 추가

--- src/main/kotlin/store/Store.kt
+++ src/main/kotlin/store/Store.kt
@@ -1,13 +1,25 @@
 package store
 
-class Store(private val products: List<Product>, private val promotion: List<Promotion>) {
+import camp.nextstep.edu.missionutils.DateTimes
+import java.time.LocalDate
+
+class Store(private val products: List<Product>, private val promotions: List<Promotion>) {
 
     fun getProducts() = products.toList()
+
+    fun isPromotionProduct(purchaseProduct: RequestProduct): Boolean {
+        val promotionStock = products.find { it.promotion != null && purchaseProduct.name == it.name } ?: return false
+        val promotion = promotions.find { it.name == promotionStock.promotion } ?: return false
+        val currentDate = DateTimes.now().toLocalDate()
+        val startDate = LocalDate.parse(promotion.startDate)
+        val endDate = LocalDate.parse(promotion.endDate)
+        return currentDate in startDate..endDate
+    }
 
     fun getApplyProduct(purchaseProduct: RequestProduct): RequestProduct {
         val promotionStock =
             products.find { product -> purchaseProduct.name == product.name } ?: return purchaseProduct.copy(count = 0)
-        val promotion = promotion.find { it.name == promotionStock.name } ?: return purchaseProduct.copy(count = 0)
+        val promotion = promotions.find { it.name == promotionStock.name } ?: return purchaseProduct.copy(count = 0)
         val totalEventCount = promotion.buy + promotion.get
         val remainPromotionCount = purchaseProduct.count % totalEventCount
         if (remainPromotionCount >= promotion.buy) {
