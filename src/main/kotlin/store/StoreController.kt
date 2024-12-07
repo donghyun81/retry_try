61cef97cab4280f6075e9b4e490ce7178605f775
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-12-07 11:44:41 +0900

    feat(StoreController): 제품들을 읽어오는 기능 구현

--- src/main/kotlin/store/StoreController.kt
+++ src/main/kotlin/store/StoreController.kt
@@ -1,9 +1,27 @@
 package store
 
+import java.io.FileReader
+
 class StoreController {
     private val outputView = OutputView()
 
     fun run() {
         outputView.printStart()
+    }
+
+    fun readProducts(): List<Product> {
+        val products = FileReader("src/main/resources/products.md").readLines().drop(1).map { productLine ->
+            val (name, price, quantity, promotion) = productLine.split(",")
+            Product(name, price.toInt(), quantity.toInt(), promotion.validateNull())
+        }.toMutableList()
+        val onlyPromotionProduct =
+            products.filter { product -> product.promotion != null && products.count { it.name == product.name } == 1 }
+        onlyPromotionProduct.forEach { product -> products.add(product.copy(quantity = 0)) }
+        return products.groupBy { it.name }.flatMap { (_, value) -> value.sortedByDescending { it.promotion } }
+    }
+
+    private fun String.validateNull(): String? {
+        if (this == "null") return null
+        return this
     }
 }
