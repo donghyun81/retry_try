b3b7ac5ed44615c1c042567d5b9431347cef1788
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-12-07 14:49:32 +0900

    refator(InputView): 에러 메시지 출력 열거형 객체로 수정

--- src/main/kotlin/store/InputView.kt
+++ src/main/kotlin/store/InputView.kt
@@ -8,13 +8,13 @@ class InputView {
         val purchaseProductInput = Console.readLine().split(",")
         println()
         val confirmedPurchaseProductInput = purchaseProductInput.map { input ->
-            require(input.first() == '[' && input.last() == ']') { "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요." }
+            require(input.first() == '[' && input.last() == ']') { Error.FORM.getMessage() }
             input.removeSurrounding("[", "]")
         }
         val purchaseProducts = confirmedPurchaseProductInput.map { input ->
-            require(input.split("-").size == 2) { "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요." }
+            require(input.split("-").size == 2) { Error.FORM.getMessage() }
             val (name, countInput) = input.split("-")
-            val count = requireNotNull(countInput.toIntOrNull()) { "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요." }
+            val count = requireNotNull(countInput.toIntOrNull()) { Error.FORM.getMessage() }
             RequestProduct(name, count)
         }.groupBy { it.name }.map { (name, product) ->
             val totalCount = product.sumOf { it.count }
@@ -22,8 +22,8 @@ class InputView {
         }
         purchaseProducts.forEach { purchaseProduct ->
             val stock = products.filter { purchaseProduct.name == it.name }
-            require(stock.isEmpty().not()) { "[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요." }
-            require(purchaseProduct.count <= stock.sumOf { it.getQuantity() }) { "[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요." }
+            require(stock.isEmpty().not()) { Error.EMPTY_PRODUCT.getMessage() }
+            require(purchaseProduct.count <= stock.sumOf { it.getQuantity() }) { Error.OVER_QUANTITY.getMessage() }
         }
         return purchaseProducts
     }
@@ -32,7 +32,7 @@ class InputView {
         println("현재 ${applyProduct.name}은(는) ${applyProduct.count}개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)")
         val isApplyProductInput = Console.readLine()
         println()
-        require(isApplyProductInput == "Y" || isApplyProductInput == "N")
+        require(isApplyProductInput == "Y" || isApplyProductInput == "N") { Error.OTHER.getMessage() }
         return isApplyProductInput == "Y"
     }
 
@@ -40,21 +40,21 @@ class InputView {
         println("현재 ${excludeProduct.name} ${excludeProduct.count}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)")
         val isExcludeProduct = Console.readLine()
         println()
-        require(isExcludeProduct == "Y" || isExcludeProduct == "N")
+        require(isExcludeProduct == "Y" || isExcludeProduct == "N") { Error.OTHER.getMessage() }
         return isExcludeProduct == "N"
     }
 
     fun readIsMembership(): Boolean {
         println("멤버십 할인을 받으시겠습니까? (Y/N)")
         val isMemberShip = Console.readLine()
-        require(isMemberShip == "Y" || isMemberShip == "N")
+        require(isMemberShip == "Y" || isMemberShip == "N") { Error.OTHER.getMessage() }
         return isMemberShip == "Y"
     }
 
     fun readIsRetryPurchase(): Boolean {
         println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)")
         val isRetryPurchase = Console.readLine()
-        require(isRetryPurchase == "Y" || isRetryPurchase == "N")
+        require(isRetryPurchase == "Y" || isRetryPurchase == "N") { Error.OTHER.getMessage() }
         return isRetryPurchase == "Y"
     }
 }
