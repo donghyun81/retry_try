6b69343beaf7bc0df415566ffbbf0f22622ef6ac
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-12-13 11:46:55 +0900

    feat(RetryUtils): 입력 재시도 기능 추가

--- src/main/kotlin/store/StoreController.kt
+++ src/main/kotlin/store/StoreController.kt
@@ -11,7 +11,7 @@ class StoreController {
         val promotion = fileService.readPromotions()
         val storeService = StoreService(products, promotion)
         outputView.printStocks(storeService.getProducts())
-        val purchaseProducts = inputView.readPurchaseProducts(storeService.getProducts())
+        val purchaseProducts = retryInput { inputView.readPurchaseProducts(storeService.getProducts()) }
         val purchaseResult = purchaseProducts.map { purchaseProduct -> storeService.getPurchaseResult(purchaseProduct) }
         outputView.printReceipt(purchaseResult, 0)
     }
