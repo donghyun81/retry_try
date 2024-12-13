9514c3cb10c3c78adc93d0f4d258920f965ebea4
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-12-13 10:56:46 +0900

    feat(StoreController): 상품 정보들을 출력하는 기능 구현

--- src/main/kotlin/store/StoreController.kt
+++ src/main/kotlin/store/StoreController.kt
@@ -2,8 +2,12 @@ package store
 
 class StoreController {
     private val outputView = OutputView()
+    private val fileService = FileService()
 
     fun run() {
         outputView.printStart()
+        val products = fileService.readProducts()
+        val promotion = fileService.readPromotions()
+        outputView.printStocks(products)
     }
 }
