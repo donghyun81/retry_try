5448d9fa15bafc86bcec6a4dce40213874c5d137
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-12-13 11:30:47 +0900

    feat(OutputView): 구매 영수증을 출력하는 기능 추가

--- src/main/kotlin/store/OutputView.kt
+++ src/main/kotlin/store/OutputView.kt
@@ -8,11 +8,31 @@ class OutputView {
         println("안녕하세요. W편의점입니다.\n")
     }
 
-    //- 콜라 1,000원 10개 탄산2+1
     fun printStocks(products: List<Product>) {
         products.forEach { product ->
             println("- ${product.name} ${product.price.wonFormat()}원 ${product.quantity.emptyStock()} ${product.promotion ?: ""}")
         }
+    }
+    
+    fun printReceipt(purchaseResults: List<PurchaseResult>, memberShipDiscount: Int) {
+        println("===========W 편의점=============")
+        println("상품명\t\t수량\t금액")
+        purchaseResults.forEach { purchaseResult ->
+            println("${purchaseResult.purchaseProduct.name}\t\t${purchaseResult.purchaseProduct.count} \t${purchaseResult.totalPrice.wonFormat()}")
+        }
+        println("===========증\t정=============")
+        purchaseResults.forEach { purchaseResult ->
+            if (purchaseResult.applyCount > 0) {
+                println("${purchaseResult.purchaseProduct.name}\t\t${purchaseResult.applyCount}")
+            }
+        }
+        val totalPrice = purchaseResults.sumOf { it.totalPrice }
+        val eventDiscount = purchaseResults.sumOf { it.applyDiscount }
+        println("==============================")
+        println("총구매액\t\t${purchaseResults.sumOf { it.purchaseProduct.count }}\t${purchaseResults.sumOf { it.totalPrice }}")
+        println("행사할인\t\t\t-${eventDiscount}")
+        println("멤버십할인\t\t\t-${memberShipDiscount}")
+        println("내실돈\t\t\t ${totalPrice - eventDiscount - memberShipDiscount}")
     }
 
     private fun Int.wonFormat(): String {
